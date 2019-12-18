4410858ce91fac6802ceb6519f3a4abf363a65e2
Lissa <A.Mosh15@yandex.ru>
2019-12-18 14:36:41 +0500

    Updates

---
+++ precache-manifest.ab9cbd7f7be868ced14c3cd611e89680.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20db7c8cb8c5b13a32f8c5d5e28d84ec",
+    "url": "/Counter/index.html"
+  },
+  {
+    "revision": "b774da68c90b3c99bd99",
+    "url": "/Counter/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e23f7476ab4c030b1c4a",
+    "url": "/Counter/static/js/2.8cc143db.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Counter/static/js/2.8cc143db.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b774da68c90b3c99bd99",
+    "url": "/Counter/static/js/main.53605f47.chunk.js"
+  },
+  {
+    "revision": "c6bd15994d908e02e37a",
+    "url": "/Counter/static/js/runtime-main.00bcd7ae.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Counter/static/media/logo.5d5d9eef.svg"
+  }
+]);
